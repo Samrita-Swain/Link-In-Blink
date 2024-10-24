665a8899cb3dbd2eaa8112fe9cf5681583d6e2fe
Samrita-Swain <samritaswain28@gmail.com>
2024-10-24 15:55:55 +0530

    Change in text

--- src/Components/Mainpage.jsx
+++ src/Components/Mainpage.jsx
@@ -7,7 +7,7 @@ const Mainpage = () => {
       <div className=" content">
         <div className="first">
           <div className="first-content"><h1>Welcome To</h1></div>
-          <div className="second-content"><h1>Link In Blink</h1></div>
+          <div className="second-content"><h1>Link in blink</h1></div>
         </div>
       </div>
       </div>
